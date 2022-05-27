d1f2e014f07dd764c73bce2d5d056ac00afadeb0
criptocore <coremanrique@gmail.com>
2022-05-27 02:00:01 -0500

    a:4

---
+++ card-core/js/index.js
@@ -0,0 +1,15 @@
+// Metodo alert()
+// alert('Hola Mundo');
+// let miVariable = 45;
+// alert(miVariable);
+// alert('número: '+miVariable);
+
+//Metodo prompt()
+// let respuesta;
+// respuesta = prompt ('¿cual es tu color favorito?:', 'Indica tu color favorito');
+// alert('Tu respuesta es: '+respuesta);
+
+//Metodo confirm
+// let respuesta2 = confirm('¿deseas salir de esta página?');
+// alert(respuesta2);
+
